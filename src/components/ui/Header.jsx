4233484f6fe88c17b85ae5fc185eb83c34bf2b89
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-05-11 18:53:49 +0530

    added header part

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,3 +1,27 @@
-export const Header=()=>{
-    return <h1>Header</h1>
-}
+import { NavLink } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container grid navGrid-col2 ">
+        <NavLink to={"/"}>
+          <h1> WorldAtlas</h1>
+        </NavLink>
+        <ul className="flex">
+          <li>
+            <NavLink to={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/about"}>About</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/country"}>Country</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/contact"}>Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
