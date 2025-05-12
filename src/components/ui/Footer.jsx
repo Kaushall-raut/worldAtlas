8f3e138141f93beb18b09b3ce5ead67d30c09939
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-05-12 23:51:44 +0530

    footer added

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,3 +1,33 @@
+import { MdPlace } from "react-icons/md";
+import footerData from "../../api/footerData.json";
+import { IoCallSharp } from "react-icons/io5";
+import { TbMailPlus } from "react-icons/tb";
+
 export const Footer = () => {
-  return <h1>Footer</h1>;
+  const footerIcon = {
+    MdPlace: <MdPlace />,
+    IoCallSharp: <IoCallSharp />,
+    TbMailPlus: <TbMailPlus />,
+  };
+
+  return (
+    <footer>
+      <section className="grid grid-col3  container footer-section">
+        {footerData.map((value, index) => {
+          const { icon, title, details } = value;
+          return (
+            <div key={index} className="footer-content">
+              <div className="icon">
+                <span>{footerIcon[icon]}</span>
+              </div>
+              <div className="footer-text">
+                <span className="title">{title}:</span>
+                <span>{details}</span>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </footer>
+  );
 };
