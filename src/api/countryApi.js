624b3bcecd1cf32a143a34d26b38fc478d73bf17
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-05-13 13:35:09 +0530

    dynamice routing

--- src/api/countryApi.js
+++ src/api/countryApi.js
@@ -7,3 +7,8 @@ const api = axios.create({
 export const getData = () => {
   return api.get("/all");
 };
+
+export const getOneCountryData = (name) =>
+  api.get(
+    `/name/${name}?fields=name,population,region,subregion,capital,tld,currencies,languages,borders,flags`
+  );
--- src/components/CountryDetail.jsx
+++ src/components/CountryDetail.jsx
@@ -1,7 +1,71 @@
+import { useEffect, useState, useTransition } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Loader } from "./ui/Loader";
+import { getOneCountryData } from "../api/countryApi";
+
 export const CountryDetail = () => {
+  const name = useParams();
+
+  const nav = useNavigate();
+
+  const [isPending, startTransition] = useTransition();
+  const [countryData, setData] = useState();
+
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getOneCountryData(name.name);
+
+      setData(res.data[0]);
+      // console.log(res.data);
+    });
+  }, []);
+  console.log(countryData);
+
+  if (isPending) return <Loader />;
   return (
-    <>
-      <h1>HEllo</h1>
-    </>
+    <section className="container  ">
+      {countryData && (
+        <div className="grid grid-col2 card-container">
+          <div>
+            <img src={countryData.flags.svg} alt="flag" />
+          </div>
+          <div>
+            <h4>{countryData.name.official}</h4>
+            <p>
+              Native name:{" "}
+              <span>
+                {Object.keys(countryData.name.nativeName).map(
+                  (name) => countryData.name.nativeName[name].common
+                )}
+              </span>
+            </p>
+            <p>
+              Currency name:{" "}
+              <span>
+                {Object.keys(countryData.currencies)
+                  .map((curElem) => countryData.currencies[curElem].name)
+                  .join(", ")}
+              </span>
+            </p>
+            <p>
+              Capital: <span>{countryData.capital[0]}</span>
+            </p>
+            <p>
+              Region: <span>{countryData.region}</span>
+            </p>
+            <p>
+              Population: <span>{countryData.population}</span>
+            </p>
+            <p>
+              Sub region: <span>{countryData.subregion}</span>
+            </p>
+            <p>
+              Domain: <span>{countryData.tld[0]}</span>
+            </p>
+          </div>
+          <button onClick={() => nav(-1)}>Go back</button>
+        </div>
+      )}
+    </section>
   );
 };
